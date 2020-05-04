60af21249a8ce37632928018d51c307c30251603
Ashish-Dalvi <ashish.dalvi00211@gmail.com>
2020-05-04 18:43:56 +0530

    adding to github

---
+++ src/test/java/com/radical/linuxacademy/LinuxacademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radical.linuxacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxacademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
